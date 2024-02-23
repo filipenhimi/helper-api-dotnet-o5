295c8b5e0f1bbbd0c3445bdef1f71e06c8e3145d
wcipriano <wagner@solucionarti.com.br>
2024-02-23 01:02:05 -0300

    :pencil: Geral: SwaggerDoc

--- helper-api-dotnet-o5/Program.cs
+++ helper-api-dotnet-o5/Program.cs
@@ -1,12 +1,43 @@
+using System.Reflection;
+using Microsoft.OpenApi.Models;
+using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 DotNetEnv.Env.Load();
 
 // Add services to the container.
-builder.Services.AddControllers();
+builder.Services.AddControllers().AddJsonOptions(options =>
+    options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter())
+);
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = "PIES - Projeto Integrado em Engenharia de Software",
+        Description = "Projeto da disciplina de PIES - API REST",
+        TermsOfService = new Uri("https://github.com/filipenhimi/helper-api-dotnet-o5/"),
+        Contact = new OpenApiContact
+        {
+            Name = "Turma 5 ENSF",
+            Email = string.Empty,
+            Url = new Uri("https://github.com/wcipriano/"),
+        },
+        License = new OpenApiLicense
+        {
+            Name = "Use under LICX",
+            Url = new Uri("https://www.endorlabs.com/learn/open-source-licensing-simplified-a-comparative-overview-of-popular-licenses"),
+        }
+    });
+
+    // Set the comments path for the Swagger JSON and UI.
+    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+    c.IncludeXmlComments(xmlPath);
+});
 
 var app = builder.Build();
 
