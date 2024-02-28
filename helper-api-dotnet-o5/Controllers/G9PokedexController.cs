c39953e81e1bc8b9fea0a3f763e5f0d77521159c
CarlosHMoura <carlosmoura@live.com>
2024-02-28 19:27:28 -0300

    Create G9Pokedex Controller

---
+++ helper-api-dotnet-o5/Controllers/G9PokedexController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace helper_api_dotnet_o5.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class G9PokedexController : ControllerBase
+{
+}
