0c75c11408161961d7f5f2fac1c36e78ff465e56
andre.aquilau <andre.aquilau@outlook.com>
2024-02-27 21:23:38 -0400

    feat: Organizando Projeto Grupo 04

--- helper-api-dotnet-o5/Program.cs
+++ helper-api-dotnet-o5/Program.cs
@@ -1,3 +1,6 @@
+using helper_api_dotnet_o5.Services;
+using helper_api_dotnet_o5.Util.Interfaces;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//DI
+builder.Services.AddScoped<ISimulacaoInvestimentoService, SimulacaoInvestimentoService>();
 
 var app = builder.Build();
 
---
+++ helper-api-dotnet-o5/Util/Class/Investimento.cs
@@ -0,0 +1,7 @@
+﻿namespace helper_api_dotnet_o5.Util.Class;
+
+public class Investimento
+{
+    public Decimal taxaAplicada { get; set; } = 0.0000M;
+    public Decimal jurosPago { get; set; } = 0.00M;
+}
