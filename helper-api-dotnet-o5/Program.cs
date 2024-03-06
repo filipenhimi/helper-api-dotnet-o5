c19eb2f7c0dfc4e6d3498b23eb4ab0a1696b7b49
wcipriano <wagner@solucionarti.com.br>
2024-03-06 19:54:45 -0300

    :ambulance: fixing previous error when was merge main into grupo12

--- helper-api-dotnet-o5/Program.cs
+++ helper-api-dotnet-o5/Program.cs
@@ -38,3 +38,20 @@ builder.Services.AddSwaggerGen(c =>
     var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
     c.IncludeXmlComments(xmlPath);
 });
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.	
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
