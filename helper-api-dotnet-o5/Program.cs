162bb04f9833d53b5acde2a75f2de1d9da5fb340
andre.aquilau <andre.aquilau@outlook.com>
2024-03-06 22:01:06 -0400

    add swagger controller

--- helper-api-dotnet-o5/Program.cs
+++ helper-api-dotnet-o5/Program.cs
@@ -1,5 +1,7 @@
 using helper_api_dotnet_o5.Services;
 using helper_api_dotnet_o5.Util.Interfaces;
+using Microsoft.OpenApi.Models;
+using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,7 +10,21 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = "Investimento CDI API",
+        Description = "An ASP.NET Core Web API for Simulação Investimento CDI",
+    });
+
+    // using System.Reflection;
+    var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFilename);
+    options.IncludeXmlComments(xmlPath);
+
+});
 
 //DI
 builder.Services.AddScoped<ISimulacaoInvestimentoService, SimulacaoInvestimentoService>();
--- helper-api-dotnet-o5/Util/DTOs/SimulacaoInvestimentoDTO.cs
+++ helper-api-dotnet-o5/Util/DTOs/SimulacaoInvestimentoDTO.cs
@@ -1,9 +1,29 @@
 ﻿namespace helper_api_dotnet_o5.Util.DTOs;
 
+
 public class SimulacaoInvestimentoDTO
 {
+    /// <summary>
+    /// Valor Investido
+    /// </summary>
+    /// <example>5000.00</example>
     public Decimal Investido { get; set; } = 0.00M;
+
+    /// <summary>
+    /// Juros Pago
+    /// </summary>
+    /// <example>400.00</example>
     public Decimal Juros { get; set; } = 0.00M;
+
+    /// <summary>
+    /// Valor total (Valor Investido + Juros)
+    /// </summary>
+    /// <example>5400.00</example>
     public Decimal ValorTotal { get; set; } = 0.00M;
+
+    /// <summary>
+    /// Taxa Aplicada
+    /// </summary>
+    /// <example>10.54216</example>
     public Decimal TaxaAplicada { get; set; } = 0.0000M;
 }
