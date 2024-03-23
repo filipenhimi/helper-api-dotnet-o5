438047f6bb1f4eb90aa252a8c848bf416307901d
CarlosHMoura <carlosmoura@live.com>
2024-03-23 19:38:01 -0300

    Ajustes Swagger.

--- helper-api-dotnet-o5/Program.cs
+++ helper-api-dotnet-o5/Program.cs
@@ -1,4 +1,6 @@
 using helper_api_dotnet_o5.Services;
+using Microsoft.OpenApi.Models;
+using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,6 +12,27 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = "PIES - Projeto Integrado em Engenharia de Software",
+        Description = "Projeto da disciplina de PIES - API REST",
+        TermsOfService = new Uri("https://github.com/filipenhimi/helper-api-dotnet-o5/"),
+        License = new OpenApiLicense
+        {
+            Name = "Use under LICX",
+            Url = new Uri("https://www.endorlabs.com/learn/open-source-licensing-simplified-a-comparative-overview-of-popular-licenses"),
+        }
+    });
+
+    // Set the comments path for the Swagger JSON and UI.
+    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+    c.IncludeXmlComments(xmlPath);
+});
 
 var app = builder.Build();
 
